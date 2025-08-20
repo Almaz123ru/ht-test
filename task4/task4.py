b3610344cf70ea324d7d0b0a07b8cd6ba3381340
Almaz123ru <almaz.islamov.2003@mail.ru>
2025-08-20 22:39:00 +0500

    Точно финал, исправил косяк в нахождении наименьшей разницы со средним числом

--- task4/task4.py
+++ task4/task4.py
@@ -17,9 +17,12 @@ for i in nums:
     mean += i
 
 mean /= len(nums)
+sorted_num = sorted(nums, reverse=True)
+absolute = 10000000000000000000000000000000000000000000000000000
 
-for i in nums:
-    if abs(mean - i) < abs(lesser_m - i) or lesser_m == -1:
+for i in sorted_num:
+    if abs(mean - i) < absolute or lesser_m == -1:
+        absolute = abs(mean - i)
         lesser_m = i
 
 mean = lesser_m
@@ -28,16 +31,10 @@ for i in range(len(nums)):
     while nums[i] != mean and max_steps != 0:
         max_steps -= 1
         if nums[i] > mean:
-            print(max_steps, "   -   ")
             nums[i] -= 1
-            # break
         elif nums[i] < mean:
-            print(max_steps, "   +   ")
             nums[i] += 1
-            # break
-        print(max_steps)
 
-print(nums)
 if max_steps != 0:
     print(20 - max_steps)
 else:
